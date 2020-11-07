0807e2508a610367666ae8fc808a5a2a23ba3991
EvaMaeRey <evangeline.mae@gmail.com>
2020-11-07 16:02:13 -0500

    blog post list

--- an_mtcars_flipbook.R
+++ an_mtcars_flipbook.R
@@ -5,12 +5,12 @@ mtcars %>%
   ggplot() +
   aes(x = wt) +
   aes(y = factor(cyl)) +
+  ggthemes::theme_solarized() +
   geom_boxplot(fill = "snow2") +
   geom_jitter(height = .2,
               alpha = .7,
               color = "plum3",
               size = 3) +
-  ggthemes::theme_solarized() +
   labs(title = "How do number of cylanders and car weight relate?") +
   labs(subtitle = "Yet another look at mtcars") +
   labs(y = "cylinder") + 
@@ -18,13 +18,13 @@ mtcars %>%
   theme(plot.title.position = "plot") +
   geom_label(data = tibble(x = 1), # one row dataframe
              fill = "grey90",
-             x = 4.9, 
+             x = 4.6, 
              y = 1.2, # cylinder is a factor w 3 levels
              label = "These observations aren't part of the 'wiskers' of the Tukey box plot. Outliers are observations further than 1.5 IQR from the upper or lower hinge" %>% 
                str_wrap(25)) +
   geom_curve(data = tibble(x = 1),
              curvature = -.2,
              angle = 80,
-             x = 4.9, xend = 5.2,
-             y = 1.9, yend = 2.8,
+             x = 4.55, xend = 5.15,
+             y = 1.85, yend = 2.8,
              )
